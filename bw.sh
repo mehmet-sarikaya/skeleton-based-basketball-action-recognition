f32661dafd4060a562a218204c85cb68c6b96518
sarikaym <mehmet.sarikaya@student.reutlingen-university.de>
2026-01-22 23:44:50 +0100

    added new files

---
+++ bw.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+#SBATCH --partition=gpu_8       # Wähle die Partition mit GPUs
+#SBATCH --gres=gpu:1            # Fordere 2 GPUs an
+#SBATCH --time=48:00:00         # Setze die maximale Laufzeit (hier 2 Stunden)
+#SBATCH --mem=180G              # Setze den benötigten Speicher
+#SBATCH --cpus-per-task=16      # Anzahl CPUs pro Task
+#SBATCH --job-name=device_combis          # Setze den Job-Namen
+#SBATCH --output=%x_%j.out     # Datei für die Ausgabe (mit Job-ID)
+
+export MODULEPATH=/opt/bwhpc/common/modulefiles/Core:$MODULEPATH
+
+# Lade die gleichen Module wie im JupyterHub
+module load jupyter/ai
+
+# Setze das Arbeitsverzeichnis auf dein Hauptverzeichnis
+cd /pfs/data5/home/hr/hr_hr/hr_sarikaym/Basketball_Vision
+
+# Debugging: Zeige das aktuelle Arbeitsverzeichnis
+echo "Current working directory: $(pwd)"
+
+# Überprüfe, ob das Python-Skript existiert
+ls -l BasketballGestureRecognitionApp.py
+
+# Setze den PYTHONPATH (falls benötigt)
+export PYTHONPATH=/pfs/data5/home/hr/hr_hr/hr_sarikaym/Basketball_Vision:$PYTHONPATH
+
+
+srun python3 BasketballGestureRecognitionApp.py
